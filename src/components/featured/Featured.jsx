7fa85b32aa926b9b8f768a4cc95d3bf56afdd5c7
nguyenjh <122562684+nguyenjh@users.noreply.github.com>
2024-07-06 21:45:16 -0700

    Created homepage lists for featured cities, property types, and featured properties

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,39 @@
+import "./featured.css"
+
+const Featured = () => {
+    return (
+        <div className="featured">
+            <div className="featuredItem">
+                <div className="featuredImgContainer">
+                    <img src="https://images.unsplash.com/photo-1448317846460-907988886b33?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="New York Img" className="featuredImg" />
+                </div>
+                <div className="featuredTitles">
+                    <h1>New York</h1>
+                    <h2>123 properties</h2>
+                </div>
+            </div>
+
+            <div className="featuredItem">
+                <div className="featuredImgContainer">
+                    <img src="https://images.pexels.com/photos/460672/pexels-photo-460672.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="London Img" className="featuredImg" />
+                </div>
+                <div className="featuredTitles">
+                    <h1>London</h1>
+                    <h2>456 properties</h2>
+                </div>
+            </div>
+
+            <div className="featuredItem">
+                <div className="featuredImgContainer">
+                    <img src="https://images.pexels.com/photos/1344537/pexels-photo-1344537.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Tokyo Img" className="featuredImg" />
+                </div>
+                <div className="featuredTitles">
+                    <h1>Tokyo</h1>
+                    <h2>789 properties</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Featured
