65fe59b231e984f0bfd1ed83d98edd985559ca93
nguyenjh <122562684+nguyenjh@users.noreply.github.com>
2024-07-07 16:21:58 -0700

    Created email list and footer components

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,53 @@
+import "./footer.css"
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="fLists">
+                <ul className="fList">
+                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regions</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districts</li>
+                    <li className="fListItem">Airports</li>
+                    <li className="fListItem">Hotels</li>
+                </ul>
+
+                <ul className="fList">
+                    <li className="fListItem">Homes</li>
+                    <li className="fListItem">Apartments</li>
+                    <li className="fListItem">Resorts</li>
+                    <li className="fListItem">Villas</li>
+                    <li className="fListItem">Hostels</li>
+                    <li className="fListItem">Guest Houses</li>
+                </ul>
+
+                <ul className="fList">
+                    <li className="fListItem">Unique places to stay</li>
+                    <li className="fListItem">All destinations</li>
+                    <li className="fListItem">All flight destinations</li>
+                    <li className="fListItem">All car rental locations</li>
+                    <li className="fListItem">All vacation destinations</li>
+                </ul>
+
+                <ul className="fList">
+                    <li className="fListItem">Car rental</li>
+                    <li className="fListItem">Flight finder</li>
+                    <li className="fListItem">Restaurant reservations</li>
+                </ul>
+
+                <ul className="fList">
+                    <li className="fListItem">Coronavirus (COVID-19) FAQs</li>
+                    <li className="fListItem">About Travel Serenity</li>
+                    <li className="fListItem">Customer Service Help</li>
+                    <li className="fListItem">Sustainability</li>
+                    <li className="fListItem">Careers</li>
+                    <li className="fListItem">Terms & Conditions</li>
+                </ul>
+            </div>
+            <div className="fText">Copyright © 2024 Travel Serenity.</div>
+        </div>
+    )
+}
+
+export default Footer
