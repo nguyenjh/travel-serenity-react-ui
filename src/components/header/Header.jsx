b2dde5e0f855c2ff36a7c32bef4132c89c75d99c
nguyenjh <122562684+nguyenjh@users.noreply.github.com>
2024-07-16 23:00:55 -0700

    Used React Context API to store search data inputs from the user's selection options on the homepage to translate them into the individual hotel page to update hotel prices based on date range and amount of rooms selected

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,19 @@
 import "./header.css"
 import { faBed, faCalendarDays, faCar, faPerson, faPlane, faTaxi } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { useState } from "react"
+import { useContext, useState } from "react"
 import { DateRange } from 'react-date-range';
 import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { format } from "date-fns"
 import { useNavigate } from "react-router-dom";
+import { SearchContext } from "../../context/SearchContext";
 
 const Header = ({type}) => {
     // calender function
     const [destination, setDestination] = useState("")
     const [openDate, setOpenDate] = useState(false)
-    const [date, setDate] = useState([
+    const [dates, setDates] = useState([
         {
           startDate: new Date(),
           endDate: new Date(),
@@ -37,8 +38,11 @@ const Header = ({type}) => {
         }})
     }
 
+    const {dispatch} = useContext(SearchContext)
+
     const handleSearch = ()=>{
-        navigate("/hotels", {state:{destination,date,options}})
+        dispatch({type:"NEW_SEARCH", payload:{destination,dates,options}})
+        navigate("/hotels", {state:{destination,dates,options}})
     }
 
     return (
@@ -94,14 +98,14 @@ const Header = ({type}) => {
                     <div className="headerSearchItem">
                         <FontAwesomeIcon icon={faCalendarDays} className="headerIcon"/>
                         <span onClick={()=>setOpenDate(!openDate)} className="headerSearchText">{`${format(
-                            date[0].startDate, "MM/dd/yyyy")} 
-                            to ${format(date[0].endDate, "MM/dd/yyyy")}`}
+                            dates[0].startDate, "MM/dd/yyyy")} 
+                            to ${format(dates[0].endDate, "MM/dd/yyyy")}`}
                         </span>
                         {openDate && <DateRange
                             editableDateInputs={true}
-                            onChange={item => setDate([item.selection])}
+                            onChange={item => setDates([item.selection])}
                             moveRangeOnFirstSelection={false}
-                            ranges={date}
+                            ranges={dates}
                             className="date"
                             minDate={new Date()}
                         />}
--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -5,9 +5,10 @@ import MailList from "../../components/mailList/MailList.jsx"
 import Footer from "../../components/footer/Footer.jsx"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCircleArrowLeft, faCircleArrowRight, faCircleXmark, faLocationDot } from "@fortawesome/free-solid-svg-icons"
-import { useState } from "react"
+import { useContext, useState } from "react"
 import useFetch from "../../hooks/useFetch.js"
 import { useLocation } from "react-router-dom"
+import { SearchContext } from "../../context/SearchContext.js"
 
 const Hotel = () => {
     const location = useLocation()
@@ -17,7 +18,18 @@ const Hotel = () => {
     const[slideNum, setSlideNum] = useState(0)
     const[open, setOpen] = useState(false)
 
-    const {data,loading,error} = useFetch(`/hotels/find/${id}`)
+    const {data, loading, error} = useFetch(`/hotels/find/${id}`)
+
+    const {dates, options} = useContext(SearchContext)
+    
+    const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
+    function dayDifference(date1, date2) {
+        const timeDiff = Math.abs(date2.getTime() - date1.getTime());
+        const diffDays = Math.ceil(timeDiff / MILLISECONDS_PER_DAY);
+        return diffDays;
+    }
+
+    const days = (dayDifference(dates[0].endDate, dates[0].startDate))
 
     const handleOpen = (i)=>{
         setSlideNum(i)
@@ -80,13 +92,13 @@ const Hotel = () => {
                             </p>
                         </div>
                         <div className="hotelDetailsPrice">
-                            <h1>Perfect for a 9-night stay!</h1>
+                            <h1>Perfect for a {days}-night stay!</h1>
                             <span>
                                 Lorem ipsum dolor sit amet consectetur adipisicing, 
                                 this property has an excellent score of 8.9!
                             </span>
                             <h2>
-                                <b>$1107</b> (9 nights)
+                                <b>${days * data.cheapestPrice * options.room}</b> ({days} nights)
                             </h2>
                             <button>Reserve or Book Now!</button>
                         </div>
