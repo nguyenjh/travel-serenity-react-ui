d059948421e89f0b081bb10fea77803a708a2674
nguyenjh <122562684+nguyenjh@users.noreply.github.com>
2024-07-19 14:45:11 -0700

    Added checkboxes to room selection menu on individual hotel page. When user selects a hotel's room, add room's id to array of selectedRooms and vice versa when room is unselected. Used React Context API to get user's date input from homepage to check the number of days that room should be reserved for. Started working on basic unavailable room reserve system

--- src/components/reserve/Reserve.jsx
+++ src/components/reserve/Reserve.jsx
@@ -2,9 +2,46 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import "./reserve.css"
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons"
 import useFetch from "../../hooks/useFetch"
+import { useContext, useState } from "react"
+import { SearchContext } from "../../context/SearchContext"
 
 const Reserve = ({setOpen, hotelId}) => {
+    const [selectedRooms, setSelectedRooms] = useState([])
     const {data, loading, error} = useFetch(`/hotels/room/${hotelId}`)
+    const {dates} = useContext(SearchContext)
+
+    const getDatesInRange = (startDate,endDate)=>{
+        const start = new Date(startDate)
+        const end = new Date(endDate)
+        const date = new Date(start.getTime())
+        let list = []
+
+        while (date <= end) {
+            list.push(new Date(date).getTime())
+            date.setDate(date.getDate()+1)
+        }
+        return list
+    }
+    const allDates = getDatesInRange(dates[0].startDate, dates[0].endDate)
+
+    const isAvailable = (roomNumber)=>{
+        const isFound = roomNumber.unavailableDates.some(date=>allDates.includes(new Date(date).getTime()))
+        return !isFound
+    }
+
+    const handleSelect = (e)=>{
+        // checks if it has been selected or not
+        const checked = e.target.checked
+        // value is room's id
+        const value = e.target.value
+        // if room is selected, add to array of prev selectedRooms
+        // if room not selected, filter out that room
+        setSelectedRooms(checked ? [...selectedRooms, value] : selectedRooms.filter((item)=>item !== value))
+    }
+
+    const handleClick = ()=>{
+
+    }
 
     return (
         <div className="reserve">
@@ -17,9 +54,17 @@ const Reserve = ({setOpen, hotelId}) => {
                             <div className="rTitle">{item.title}</div>
                             <div className="rDesc">{item.desc}</div>
                             <div className="rMax">Max people: <b>{item.maxPeople}</b></div>
+                            <div className="rPrice">{item.price}</div>
                         </div>
+                        {item.roomNumbers.map(roomNumber=>(
+                            <div className="room">
+                                <label>{roomNumber.number}</label>
+                                <input type="checkbox" value={roomNumber._id} onChange={handleSelect} disabled={!isAvailable(roomNumber)}/>
+                            </div>
+                        ))}
                     </div>
                 ))}
+                <button onClick={handleClick} className="rBtn">Reserve Now!</button>
             </div>
         </div>
     )
